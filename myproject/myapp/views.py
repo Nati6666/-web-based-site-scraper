49b7d7d4df9446c0801e5f82bfd78cbfdf6590cf
Nati Melkamu <natimelkemu@gmail.com>
2025-07-17 13:35:10 -0700

    Update models, views, and admin for web scraper functionality

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
+from .models import Link
 
-# Create your views here.
+
+def scrape_view(request):
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    page = requests.get('https://news.ycombinator.com', headers=headers)
+
+    soup = BeautifulSoup(page.text, 'html.parser')
+   
+    for link in soup.find_all('a'):
+        link_address = link.get('href')
+        link_text = link.string
+        Link.objects.create(address=link_address, name=link_text)
+
+    data = Link.objects.all()
+       
+    return render(request, 'myapp/result.html', {'data': data})
